d8cf82b079578954fc132255612432dc817278b4
Oleksandr Shepetko <a@shepetko.com>
2025-08-08 21:48:09 +0300

    Fix config

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,44 +12,42 @@ type StreamingDriver string
 
 const (
 	StorageDDB StorageDriver = "ddb"
-
-	StreamingKafka StreamingDriver = "kafka"
 )
 
 type Monobank struct {
-	Enabled bool   `yaml:"enabled" envconfig:"APP_MONOBANK_ENABLED"`
-	APIKey  string `yaml:"api_key" envconfig:"APP_MONOBANK_API_KEY"`
+	Enabled bool   `yaml:"enabled" envconfig:"MONOBANK_ENABLED"`
+	APIKey  string `yaml:"api_key" envconfig:"MONOBANK_API_KEY"`
 }
 
 type Currency struct {
-	List          []string `yaml:"list" envconfig:"APP_CURRENCY_LIST"`
-	RefreshPeriod int      `yaml:"refresh_period" envconfig:"APP_CURRENCY_REFRESH_PERIOD"` // in seconds
+	List          []string `yaml:"list" envconfig:"CURRENCY_LIST"`
+	RefreshPeriod int      `yaml:"refresh_period" envconfig:"CURRENCY_REFRESH_PERIOD"` // in seconds
 }
 
 type Storage struct {
-	Driver StorageDriver `yaml:"driver" envconfig:"APP_STORAGE_DRIVER"`
+	Driver StorageDriver `yaml:"driver" envconfig:"STORAGE_DRIVER"`
 	DDB    StorageDriverOptionsDDB
 }
 
 type StorageDriverOptionsDDB struct {
-	Region          string `yaml:"region" envconfig:"APP_STORAGE_DDB_REGION"`
-	TableName       string `yaml:"table_name" envconfig:"APP_STORAGE_DDB_TABLE_NAME"`
-	AccessKeyID     string `yaml:"access_key_id" envconfig:"APP_STORAGE_DDB_ACCESS_KEY_ID"`
-	AccessKeySecret string `yaml:"access_key_secret" envconfig:"APP_STORAGE_DDB_ACCESS_KEY_SECRET"`
+	Region          string `yaml:"region" envconfig:"STORAGE_DDB_REGION"`
+	TableName       string `yaml:"table_name" envconfig:"STORAGE_DDB_TABLE_NAME"`
+	AccessKeyID     string `yaml:"access_key_id" envconfig:"STORAGE_DDB_ACCESS_KEY_ID"`
+	AccessKeySecret string `yaml:"access_key_secret" envconfig:"STORAGE_DDB_ACCESS_KEY_SECRET"`
 }
 
 type StreamingDriverOptionsKafkaTopics struct {
-	Currency string `yaml:"currency" envconfig:"APP_STREAMING_KAFKA_TOPIC_CURRENCY"`
+	Currency string `yaml:"currency" envconfig:"STREAMING_KAFKA_TOPICS_CURRENCY"`
 }
 
 type StreamingOptionsKafka struct {
-	Enabled          bool                              `yaml:"enabled" envconfig:"APP_STREAMING_KAFKA_ENABLED"`
-	BootstrapServers []string                          `yaml:"bootstrap_servers" envconfig:"APP_STREAMING_KAFKA_BOOTSTRAP_SERVERS"`
-	Topics           StreamingDriverOptionsKafkaTopics `yaml:"topics" envconfig:"APP_STREAMING_KAFKA_TOPICS"`
+	Enabled          bool                              `yaml:"enabled" envconfig:"STREAMING_KAFKA_ENABLED"`
+	BootstrapServers []string                          `yaml:"bootstrap_servers" envconfig:"STREAMING_KAFKA_BOOTSTRAP_SERVERS"`
+	Topics           StreamingDriverOptionsKafkaTopics `yaml:"topics"`
 }
 
 type Streaming struct {
-	Kafka StreamingOptionsKafka `yaml:"kafka" envconfig:"APP_STREAMING_KAFKA"`
+	Kafka StreamingOptionsKafka `yaml:"kafka"`
 }
 
 type Config struct {
